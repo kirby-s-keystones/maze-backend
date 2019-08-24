cd7fd0307d9794281e9dc7e590aa46fd53a53635
Alex Yi <probesalesman@gmail.com>
2019-08-24 19:38:25 -0400

    uses innerHTML to create user mazes in pixel form

--- public/allMazes.js
+++ public/allMazes.js
@@ -3,19 +3,34 @@ const list = document.getElementById("list");
 async function getMazes() {
     try {
         const { data } = await axios.get('/api/maze');
+
         data.forEach(maze => {
             let li = document.createElement("li");
 
             li.innerHTML = `
             <div class="row">
                 <div class="col-4">
-                    ${data.}
-                </div>
                 <div class="col-8">
                     ${maze.name}
                 </div>
+                  <table>
+                  ${
+                    maze.maze.map(row => {
+                      return (
+                        `<tr>
+                        ${
+                          row.map(cell => {
+                            return (`<td class="${cell ? "black" : ""}"></td>`);
+                          }).join('')
+                        }
+                        </tr>`
+                      );
+                    }).join('')
+                  }
+                  </table>
+                </div>
             </div>
-            `
+            `;
             list.appendChild(li);
         });
     } catch (err) {
